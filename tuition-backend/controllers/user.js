f6bd621651549062be670ae7e6f794f6419eeac8
Ashish21212 <acp.aashish44@gmail.com>
2025-02-17 22:43:48 +0545

    jwt token created

--- tuition-backend/controllers/user.js
+++ tuition-backend/controllers/user.js
@@ -1,5 +1,6 @@
 const mongoose = require("mongoose");
 const bcrypt = require("bcrypt");
+const jwt = require('jsonwebtoken');
 
 const User = require("../models/user");
 
@@ -61,9 +62,23 @@ User.find({email:req.body.email})
       })
     }
     if (result){
+      // return res.status(200).json({
+      //   message: "Auth successful"
+      // })
+      const token =jwt.sign(
+        {
+          email:user[0].email,
+          userId: user[0]._id,
+        },
+        process.env.JWT_KEY,
+        {
+          expiresIn: "1h",
+        }
+      )
       return res.status(200).json({
-        message: "Auth successful"
-      })
+        message: "Auth successful",
+        token: token,
+      });
     }
     res.status(401).json({
       message:" Auth failed"
