3df8839477bc6f0d7de56a94b9d972afc7137e71
Ashish21212 <acp.aashish44@gmail.com>
2025-02-18 21:29:48 +0545

    route protection with jwt token ,creating middleware

--- tuition-backend/routes/class.js
+++ tuition-backend/routes/class.js
@@ -2,10 +2,12 @@ const express = require("express");
 const router = express.Router();
 const classController = require('../controllers/class')
 
-router.get('/class',classController.get_all_class);
+const checkAuth = require('../middleware/check-auth');
 
-router.post('/class',classController.create_class);
+router.get('/class', classController.get_all_class);
 
-router.get('/:classid', classController.get_one_class)
+router.post('/class',checkAuth,classController.create_class);
+
+router.get('/:classid',checkAuth, classController.get_one_class)
 
 module.exports = router;
--- tuition-backend/routes/user.js
+++ tuition-backend/routes/user.js
@@ -2,12 +2,13 @@ const express = require("express");
 const router = express.Router();
 
 const userController = require('../controllers/user')
+const checkAuth = require('../middleware/check-auth');
 
 
 router.post('/signup',userController.users_create_sign);
 
 router.post('/login',userController.users_login);
 
-router.delete('/:userId', userController.user_delete);
+router.delete('/:userId',checkAuth, userController.user_delete);
 
 module.exports = router;
